bf468dd399f371b6b494aa71dd12f96b59124503
Asanka Nawagamuwage <asanka.nawagamuwage@securepay.com.au>
2022-08-13 09:28:40 +1000

    Added Data Context

---
+++ ProductAPI/src/ProductAPI/Application/Contracts/Persistence/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Application.Contracts.Persistence
+{
+	public interface IApplicationDbContext
+	{
+		DbSet<Product> Products { get; set; }
+	}
+}
---
+++ ProductAPI/src/ProductAPI/Infrastructure/Persistence/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Application.Contracts.Persistence;
+using ProductAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Infrastructure.Persistence
+{
+	public class DataContext : DbContext, IApplicationDbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
