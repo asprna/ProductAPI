6183b43b451e71574ed352e22e95da6a5f811ac8
Asanka Nawagamuwage <asanka.nawagamuwage@securepay.com.au>
2022-08-13 10:31:58 +1000

    Added Product API

---
+++ ProductAPI/src/ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Controllers
+{
+	public class ProductController : ApiControllerBase
+	{
+		[HttpGet("{id}", Name = "Get Product Details")]
+		[Produces("application/json")]
+		[ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.NotFound)]
+		public async Task<IActionResult> Get(int id)
+		{
+			return Ok();
+		}
+	}
+}
