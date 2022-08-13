32ec197565b78bd57f5f305cee61f9824d45d983
Asanka Nawagamuwage <asanka.nawagamuwage@securepay.com.au>
2022-08-13 12:44:58 +1000

    Added Unit test for validation

---
+++ ProductAPI/test/ProductAPI.Test/Features/Products/Queries/GetProduct/GetProductQueryValidatorTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation.TestHelper;
+using ProductAPI.Application.Features.Products.Queries.GetProduct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProductAPI.Test.Features.Products.Queries.GetProduct
+{
+	public class GetProductQueryValidatorTest
+	{
+		private readonly GetProductQueryValidator _getProductQueryValidator;
+
+		public GetProductQueryValidatorTest()
+		{
+			_getProductQueryValidator = new GetProductQueryValidator();
+		}
+
+		[Fact]
+		public void GetProductQueryValidator_LessThanOne_ValidationError()
+		{
+			//Arrange
+			var getProductQuery = new GetProductQuery { Id = 0 };
+
+			//Act
+			var result = _getProductQueryValidator.TestValidate(getProductQuery);
+
+			//Assert
+			result.ShouldHaveValidationErrorFor(p => p.Id)
+				.WithErrorMessage("Invalid Product Id");
+		}
+
+		[Fact]
+		public void GetProductQueryValidator_GreaterThanOne_NoValidationError()
+		{
+			//Arrange
+			var getProductQuery = new GetProductQuery { Id = 1 };
+
+			//Act
+			var result = _getProductQueryValidator.TestValidate(getProductQuery);
+
+			//Assert
+			result.ShouldNotHaveAnyValidationErrors();
+		}
+	}
+}
